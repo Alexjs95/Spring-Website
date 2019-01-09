ab717311431927c23ea2580badb1640c6ce3eb1f
Alex Scotson <alexscotson@alexs-mbp.home>
2019-01-09 09:57:07 +0000

    added index controller and page

--- forum/src/main/java/Alex_Scotson/forum/ForumApplication.java
+++ forum/src/main/java/Alex_Scotson/forum/ForumApplication.java
@@ -1,6 +1,7 @@
 package Alex_Scotson.forum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- forum/src/main/java/Alex_Scotson/forum/models/Topic.java
+++ forum/src/main/java/Alex_Scotson/forum/models/Topic.java
@@ -61,6 +61,6 @@ public class Topic {
         this.message = message;
     }
 
-    
+
 
 }
