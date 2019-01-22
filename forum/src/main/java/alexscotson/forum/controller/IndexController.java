8e7f3e8b396db902e9d97f2290df4eb39778afbe
Alexjs95 <aj.scotson@hotmail.com>
2019-01-22 12:43:13 +0000

    added log out functionality and updated nav bar to include currently logged in user

--- forum/src/main/java/alexscotson/forum/controller/IndexController.java
+++ forum/src/main/java/alexscotson/forum/controller/IndexController.java
@@ -1,8 +1,14 @@
 package alexscotson.forum.controller;
 
+import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.authentication.rememberme.AbstractRememberMeServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -10,6 +16,15 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        CookieClearingLogoutHandler cookieClearingLogoutHandler = new CookieClearingLogoutHandler(AbstractRememberMeServices.SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY);
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        cookieClearingLogoutHandler.logout(request, response, null);
+        securityContextLogoutHandler.logout(request, response, null);
+        return "redirect:/";
     }
 
 //    @GetMapping("/login")
